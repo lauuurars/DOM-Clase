52757c99c0717f303fb9ce014c11611edb69b74b
lauuurars <1120098177@u.icesi.edu.co>
2025-04-07 08:51:15 -0500

    fin punto 2

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -9,6 +9,12 @@
  */
 
 
+//-------------------- SOLUCIÓN
+
+let h1 = document.querySelector('#titulo');
+h1.innerText = "Hola Mundo :3 !!!!";
+console.log(h1);
+
 
 // 2.2 Cambiar el atributo src de la imagen
 /**
@@ -17,6 +23,12 @@
  * 3. Ver el efecto en el navegador
  */
 
+//--------------------- SOLUCIÓN
+
+let img = document.querySelector('img');
+img.src = "imagen/carita.png";
+img.alt = "Carita";
+console.log(img);
 
 
 // 2.3 Cambiar la clase del div
@@ -26,7 +38,14 @@
  * 3. Ver el efecto en el navegador
  */
 
- 
+// -------------- SOLUCIÓN
+/*
+
+let div = document.querySelector('.main-div');
+div.className = "new-div";
+console.log(div);
+
+*/
 
 // 2.4 Cambiar el atributo href del enlace
 /**
@@ -42,6 +61,17 @@
  * 8. Ver el efecto en el navegador
  */
 
+//----------------- SOLUCIÓN 
+
+let enlace = document.querySelector('#enlace');
+console.log(enlace.innerHTML);
+console.log(enlace.getAttribute('href')); //muestra el contenido del atributo href
+enlace.setAttribute('href', 'https://www.icesi.edu.co'); //cambiamos el enlace usando setAttribute
+enlace.innerText = "Universidad Icesi"; //modificamos el texto del enlace
+enlace.setAttribute('target', '_blank'); //agregamos el atributo target y el valor blank
+enlace.className = ('class', 'boton'); //modificamos la clase del enlace a la clase "boton"
+enlace.removeAttribute('class'); //eliminamos la clase del enlace
+console.log(enlace); //imprimimos nuestro enlace modificado
 
 // 2.5 Cambiar el estilo del div
 /**
@@ -57,7 +87,15 @@
  * 3. Ver el efecto en el navegador
  */
 
+//---------------- SOLUCIÓN
 
-
-
+let div2 = document.querySelector('.main-div');
+div2.style.backgroundColor = "#a48fd4"; //cambiamos el color de fondo a verde
+div2.style.color = "white";
+div2.style.fontSize = "30px"; //cambiamos el tamaño de la fuente a 30px
+div2.style.fontFamily = "Arial"; //cambiamos la fuente a Arial
+div2.style.textAlign = "center"; //centramos el texto
+div2.style.padding = "20px"; //agregamos un padding de 20px
+div2.style.borderRadius = "10px"; //agregamos un border-radius de 10px
+console.log(div2);
 
