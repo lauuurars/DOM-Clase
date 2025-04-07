7981222af1abbece0619a583e3587b3290bf19fd
lauuurars <1120098177@u.icesi.edu.co>
2025-04-07 08:03:27 -0500

    fin ejercicio 1

---
+++ ejercicio1/script.js
@@ -0,0 +1,104 @@
+
+//  Ejercicio 1 - Seleccionar elementos del DOM
+
+/** 1.1. Selecionar x id
+ * 1. Seleccionar el elemento por su id "titulo"
+ * 2. Mostrar su contenido en consola
+ */
+
+/* 
+let titulo = document.getElementById('titulo');
+console.log(titulo); //se muestra el contenido html del elemento titulo
+*/
+
+/** 1.2. Seleccionar x clase
+ * 1. Seleccionar el div por su clase main-div 
+ * 2. Mostrar su contenido en consola
+ * 3. Mostrar su contenido en consola con innerHTML
+ * 4. Mostrar su contenido en consola con innerText
+ */
+
+/*
+const miDiv = document.getElementsByClassName('main-div'); //se selecciona el div por su clase main-div
+console.log(miDiv); //se muestra el contenido html del div
+*/
+
+
+/* ---------- Segunda Forma de resolver
+
+const miDiv = document.querySelector('main-div')[0]; //se selecciona el div y se muestra su primer elemento [0]
+console.log(miDiv); //se muestra el contenido html del div
+
+*/
+
+
+/* -------------- Tercera Forma de resolver 
+
+const miDiv = document.querySelector('.main-div'); //se especifica que es una clase agregando . 
+console.log(miDiv.innerHTML); //se muestra el contenido html del div (etiquetas)
+console.log(miDiv.innerText); //se muestra el contenido de texto del div sin etiquetas html
+console.log(miDiv); //se muestra el contenido html del div sin etiquetas html
+
+*/
+
+
+// 1.3. Por etiqueta
+/**
+ * 1. Seleccionar el elemento por su etiqueta "img"
+ * 2. Mostrar su contenido en consola
+ * 3. Mostrar el contenido de los atributos src y alt por consola
+ */
+
+/*
+let img = document.getElementsByTagName('img'); //se selecciona el elemento por su etiqueta img
+console.log(img); 
+*/
+
+/* --------------- Segunda Forma de resolver
+
+let img = document.getElementsByTagName('img')[0]; //se selecciona el primer elemento por su etiqueta img
+console.log(img); //se muestra el contenido html del elemento 0 de img
+
+*/
+
+/* --------------- Tercera Forma de resolver
+
+let img = document.querySelector('img'); //solo selecciona el primer elemento por su etiqueta img
+console.log(img); //se muestra el contenido html del elemento 0 de img
+
+*/
+
+/* ---------------- Cuarta forma de resolver
+
+let img = document.querySelectorAll('img'); //se selecciona todos los elementos por su etiqueta img
+console.log(img)
+
+*/
+
+// 1.4. Por querySelector
+/**
+ * 1. Seleccionar el elemento por su id "titulo"
+ * 2. Mostrar su contenido en consola
+ * 3. Seleccionar el div por su clase main-div 
+ * 4. Mostrar su contenido en consola
+ * 5. Seleccionar el elemento por su etiqueta "img"
+ * 6. Mostrar su contenido en consola
+ * 7. Mostrar el contenido de los atributos src y alt por consola
+ */
+
+//----------------- Solución 
+
+let titulo = document.querySelector('#titulo'); //se selecciona el elemento por su id titulo
+console.log(titulo);
+
+let mainDiv = document.querySelector('.main-div'); //seleccionamos el div por su clase main-div, usando el .
+console.log(mainDiv);
+
+let img = document.querySelector('img'); //seleccionamos el primer elemento por su etiqueta img
+console.log(img);
+console.log(img.src); //se muestra el contenido del atributo src de la imagen
+console.log(img.alt); //se muestra el contenido del atributo alt de la imagen
+
+
+
+
---
+++ ejercicio2/script.js
@@ -0,0 +1,63 @@
+
+// Ejercicio2: Cambiar los Atributos de los elementos del DOM
+
+// 2.1 Seleccionar elemento del DOM
+/** 
+ * 1. Seleccionar el elemento h1 con querySelector
+ * 2. Cambiar el titulo por "Hola Mundo"
+ * 3. Ver el efecto en el navegador
+ */
+
+
+
+// 2.2 Cambiar el atributo src de la imagen
+/**
+ * 1. Seleccionar la imagen con querySelector
+ * 2. Cambiar su atributo src por "imagen/carita.png"
+ * 3. Ver el efecto en el navegador
+ */
+
+
+
+// 2.3 Cambiar la clase del div
+/**
+ * 1. Seleccionar el div main-div con querySelector
+ * 2. Cambiar su clase por "new-div"
+ * 3. Ver el efecto en el navegador
+ */
+
+ 
+
+// 2.4 Cambiar el atributo href del enlace
+/**
+ * 1. Seleccionar el enlace con querySelector usando su id
+ * 2. Mostrar su contenido en consola
+ * 3. Mostrar el contenido de su atributo href por consola con getAttribute
+ * 2. Cambiar su atributo href por "https://www.icesi.edu.co"
+ * 3. Cambiar su texto por "Universidad Icesi"
+ * 4. Cambiar su atributo target por "_blank"
+ * 5. Cambiar su clase por "boton"
+ * 6. Ver el efecto en el navegador
+ * 7. Eliminar la clase del enlace
+ * 8. Ver el efecto en el navegador
+ */
+
+
+// 2.5 Cambiar el estilo del div
+/**
+ * 1. Seleccionar el div con querySelector
+ * 2. Cambiar su estilo por:
+ *    - background-color: green
+ *    - color: white
+ *    - font-size: 30px
+ *    - font-family: Arial
+ *    - text-align: center
+ *    - padding: 20px
+ *    - border-radius: 10px
+ * 3. Ver el efecto en el navegador
+ */
+
+
+
+
+
