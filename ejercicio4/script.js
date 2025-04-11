1b2000681529a53fb5928469950a2add17c06635
lauuurars <1120098177@u.icesi.edu.co>
2025-04-11 16:15:38 -0500

    se reemplazaron archivos

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,13 +1,32 @@
 
 /**
- * Ejercicio 4. Agregar y Eliminar Elementos al DOM
- *  
- * 1. Seleccionar los elementos: contenedor y boton con los que se va a trabajar
- * 2. Crear un nuevo elemento parafo con createElement en el documento
- * 3. Agregar el texto al nuevo parafo creado
- * 4. Adicionar el elemento creado al contenedor selecccioando con appendChild
- * 5. El codigo anterior incluirlo en una nueva funcion crearParafo()
- * 6. Adicionar al boton un escuchador para el evento click que  llame la funcion
- *    crear_parrafo()
- * 
+ * Ejercicio 4. Jerarquia Elementos al DOM
+ * ---------------------------------------
+ * 1. Capturar los elementos: contenedor, parrafo1 y parrafo2 con queryselector
+ * 2. Ir al nodo padre del parrafo1 y mostrar su etiqueta con tagName
+ * 3. Mostrar los hijos que tiene el elemento contenedor
+ * 3. Mostrar cuantos hijos tiene el elemento contenedor
+ * 4. Mostrar el primer hijo de el contenedor con []
+ * 5. Mostrar el Primer y último hijo con firstElementChild y lastElementChild
+ * 6. Mostrar el texto del siguiente hermano de parrafo1 nextElementSibling
+ * 7. Mostrar el id del anterior hermano de parrafo2 previousElementSibling
+ * 8. Recorrer la lista de frutas (Nodos hijos) con ayuda del DOM 
  */
+
+  // Capturar los  elementos 
+
+
+  // Nodo padre
+
+
+  // Hijos del contenedor
+
+
+  // Primer y último hijo
+
+
+  // Hermanos
+   
+
+  // Recorrer la lista de frutas con el DOM
+
