cb936781349fe65591882f1f4949478f05d3733c
lauuurars <1120098177@u.icesi.edu.co>
2025-04-11 17:09:21 -0500

    fin ejercicio 5

--- ejercicio5/script.js
+++ ejercicio5/script.js
@@ -12,6 +12,36 @@
  * 
  */
 
+//1. 
+
+const contenedor = document.querySelector('#contenedor') //seleccionamos el contenedor para añadir un nuevo elemento
+const botonAgregar = document.querySelector('#botonAgregar') //seleccionamos el boton
+const botonEliminar = document.querySelector('#botonEliminar') //seleccionamos el boton eliminar
+console.log(contenedor, botonAgregar, botonEliminar)
+
+
+let nuevoParrafo = document.createElement('p') //creamos el nuevo elemento parrafo 
+nuevoParrafo.textContent = 'Soy un nuevo párrafo :3' //agregamos el texto al nuevo parrafo creado 
+contenedor.appendChild(nuevoParrafo) //adicionamos el nuevo parrafo al contenedor seleccionado
+
+function crearParrafo () {
+    let nuevoParrafo = document.createElement('p') 
+    nuevoParrafo.innerText = 'Soy un nuevo párrafo :3'
+    contenedor.appendChild(nuevoParrafo) 
+}
+
+botonAgregar.addEventListener('click', crearParrafo) //agregamos el evento click al boton para que llame a la funcion crearParrafo
+
+//7. Eliminar el párrafo
+
+botonEliminar.addEventListener('click', function (){
+    let elementoEliminar = document.querySelector('p')
+    if (!elementoEliminar) {
+        alert("Ya no hay elementos :(") //si no hay elementos para eliminar, mostramos un mensaje de alerta
+        return //si no hay elementos para eliminar, salimos de la función
+    }
+    elementoEliminar.remove() 
+}) 
 
 
 
